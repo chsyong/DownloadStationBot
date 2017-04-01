1383c55ad8f486c3a12261fd820315f2a0393965
chsyong <chsyong2@gmail.com>
2017-04-01 17:47:34 +0900

    modified:   setup.sh

--- setup.sh
+++ setup.sh
@@ -2,3 +2,5 @@
 ./get-pip.py
 pip install BeautifulSoup
 pip install psutil  
+git clone -b master https://github.com/Diaoul/subliminal /tmp/subliminal
+cd /tmp/subliminal /bin/python setup.py install 
